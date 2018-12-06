bc2e961013cd1205d1cd2b0b5dae25b9e7a5a18f
Christian Hansen <cplay@optonline.net>
2018-12-06 15:51:14 -0500

    finished part 3

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lomabard%20street%20philadelphia&key=' + API_KEY,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
