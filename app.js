babc229345516bcaf4d1366c55fb2c01999383d8
Christian Hansen <cplay@optonline.net>
2018-12-07 10:20:18 -0500

    finished part 8

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const request = require('request');
+/*
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode')
@@ -20,5 +22,18 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
         console.log(errorMessage);
     } else {
         console.log(JSON.stringify(results, undefined, 2));
+    }
+});
+*/
+// bac5517da3e991289f61e781bbc2996d
+
+request({
+    url: 'https://api.darksky.net/forecast/bac5517da3e991289f61e781bbc2996d/40.5843576,-74.6519598',
+    json: true
+}, (error, response, body) => {
+    if(!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    } else {
+        console.log('Unable to fetch weather.');
     }
 });
